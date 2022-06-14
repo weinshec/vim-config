cd68dd94aa34863c2178b80d3e18ae56e3287655
Christoph Weinsheimer <weinshec@holodeck2.de>
2022-06-14 12:18:42 +0200

    Add :nohl binding to <c-n>

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -7,5 +7,8 @@ vim.keymap.set("", "<c-h>", "<c-w>h")
 vim.keymap.set("", "<s-h>", ":tabprevious<CR>")
 vim.keymap.set("", "<s-l>", ":tabnext<CR>")
 
+vim.keymap.set("n", "<c-n>", ":nohl<CR>")
+
 vim.keymap.set("v", "<", "<gv")
 vim.keymap.set("v", ">", ">gv")
+
